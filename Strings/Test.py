55b9ae6b5c63a8c7bf2fb1667fd74bcee70baccb
Pritam <pritam123@gmail.com>
2019-07-04 10:59:43 +0530

    null

--- Strings/Test.py
+++ Strings/Test.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 string = "Pritaammmmm"
 my_dict = {}
 
@@ -8,17 +9,15 @@ for s in string:
         my_dict[s] = 1
 
 
-print(my_dict)
+# print(my_dict)
 
-
-from collections import OrderedDict
 
 my_dict1 = OrderedDict()
 
 my_dict1['a1'] = "Test1"
 my_dict1['a2'] = "Test2"
 
-print(my_dict1)
+# print(my_dict1)
 
 arr = [1, 2, 5, 1, 7, 2, 4, 2]
 # mp = { i: 0 for i in arr}
@@ -31,8 +30,39 @@ for i in arr:
         mp[i] += 1
     else:
         mp[i] = 1
-print(mp)
-print(mp.keys())
+# print(mp)
+# print(mp.keys())
 
+lst = [2, 5, 2, 8, 5, 6, 8, 8]
+dict1 = {}
 
+for k in lst:
+    if k in dict1.keys():
+        dict1[k] += 1
+    else:
+        dict1[k] = 1
+print(dict1)
+lst_sort = sorted(dict1.values())
+print(lst_sort)
+# for k, v in dict1.items():
+#     for i in lst_sort:
+#         if i == v:
+#             for j in range(i):
+#                 print(k)
 
+# for k, v in dict1.items():
+#     if v in lst_sort:
+#         for i in range(v):
+#             print(k)
+
+# for i in lst_sort:
+#     for k, v in dict1.items():
+#         if i == v:
+#             for j in range(i):
+#                 print(k)
+
+for i in lst_sort:
+    for k, v in dict1.items():
+        if v == i:
+            for j in range(i):
+                print(k)
